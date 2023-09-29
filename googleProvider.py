425c26eed45f7ce2ea12ff6d256fa2d95c48258c
mahsabehdadnia <mahsa.behdadnia@gmail.com>
2023-09-29 16:24:56 +0200

    create project

--- googleProvider.py
+++ googleProvider.py
@@ -1,19 +1,24 @@
+
 from google.cloud import resourcemanager_v3
+from google.oauth2 import service_account
 
-def create_project():
-    client = resourcemanager_v3.ProjectsClient()
-    # Initialize request argument(s)
-    request = resourcemanager_v3.CreateProjectRequest(
-    )
 
-    # Make the request
-    operation = client.create_project(request=request)
+def create_project(project_id: str, project_name: str, parent_id: str):
 
-    print("Waiting for operation to complete...")
+    credentials = service_account.Credentials.from_service_account_file('dcevm-400513-952876057a88.json')
+    client = resourcemanager_v3.ProjectsClient(credentials=credentials)
 
-    response = operation.result()
+    create_project_request = resourcemanager_v3.CreateProjectRequest(project = {
+        "project_id": project_id,
+        "parent": parent_id,
+    })
 
-    # Handle the response
-    print(response)
+    response = client.create_project(request=create_project_request)
+    print(f"Created project: {response.name}")
 
-    
+if __name__ == '__main__':
+    project_id = "my-medium-project-id"
+    project_name = "my-medium-project"
+    parent_id = "folders/926442729531"
+
+    create_project(project_id, project_name, parent_id)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import googleProvider
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello"}
+
+@app.post("/new_project")
+def create_new_project():
+    googleProvider.create_project()
